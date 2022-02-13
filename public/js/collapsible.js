3df3e93eaefea938928b3f6524b6be06e1acfc90
Michael Klein <Michaelklein1997@gmail.com>
2022-02-12 19:50:34 -0600

    changes messages section to notes section

--- public/js/collapsible.js
+++ public/js/collapsible.js
@@ -13,3 +13,15 @@ for(let i = 0; i<collapsible.length;i++) {
         }
     })
 }
+
+for (i = 0; i < collapsible.length; i++) {
+    collapsible[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
