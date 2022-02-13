37120ed906a63cf74023b2485e98f2e942db5de2
Michael Klein <Michaelklein1997@gmail.com>
2022-02-13 17:43:03 -0600

    cleaning up the trelloBoard code

--- public/js/trelloBoard.js
+++ public/js/trelloBoard.js
@@ -12,48 +12,14 @@ const movableListText = document.querySelector('#moveableListsH6');
 // toggle.checked == false
 const toggle = document.querySelector('#draggableToggle');
 
-toggle.addEventListener('click', () => {
-    if (toggle.checked == true) {
 
-        movableTaskText.style.opacity = '50%';
-        movableListText.style.opacity = '100%';
-
-        lists = document.querySelectorAll('.list');
-        // Columns are draggable
-        lists.forEach(list => {
-            list.setAttribute('draggable', 'true');
-        });
-        // List_items are NOT draggable
-        list_items.forEach(list_item => {
-            list_item.setAttribute('draggable', 'false');
-        })
-
-        container.addEventListener('dragover', dropList)
-        listDrag()
-    } else if (toggle.checked == false) {
-
-        movableTaskText.style.opacity = '100%';
-        movableListText.style.opacity = '50%';
-
-        // Columns are NOT draggable
-        lists.forEach(list => {
-            list.setAttribute('draggable', 'false')
-        })
-        // List_items are draggable
-        list_items.forEach(list_item => {
-            list_item.setAttribute('draggable', 'true');
-        })
-
-        container.removeEventListener('dragover', dropList)
-        makeDraggable()
-    }
-})
 
 let draggedItem = null;
 
+
+// USER CLICKS ADD NEW LIST AND A NEW LIST APPENDS TO PAGE
 const addList = () => {
     const newList = document.createElement('div');
-
     newList.innerHTML = `<div class="d-flex justify-content-between align-items-center moveList"><h3 class="text-white listTitle">Click to edit</h3><img src="./img/pencil_icon.png" id="editListButton"></div><div class="taskList"></div><h5 class="text-white addTaskButton"><span class="bold">+</span> Add task</h5>`
     newList.classList.add('list', 'd-flex', 'flex-column', 'gap-2')
 
@@ -70,16 +36,12 @@ const addList = () => {
     }
 
     document.querySelector('#trelloBoard').appendChild(newList)
-    // let lists = document.querySelectorAll('.list');
-    console.log(list_items)
-    addTaskButton = document.querySelectorAll('.addTaskButton');
-  
 
-    addTaskButton[addTaskButton.length - 1].addEventListener('click', appendTask)
+    //ADD EVENT LISTENER TO NEWLY ADDED LIST'S ADD TASK BUTTON
+    newList.children[2].addEventListener('click', appendTask)
 
-
-    //NEEDED FOR TOGGLING LIST ITEM DRAG -- DONT DELETE
-    list_items = document.querySelectorAll('.list-item')
+    // IF TOGGLE FOR DRAGGING LISTS IS ENABLED THEN ADD DRAG EVENT LISTENER TO NEWLY ADDED LIST ELEMENT
+    // IF TOGGLE FOR DRAGGING LISTS IS DISABLED THEN ADD DRAG EVENT LISTENER TO NEWLY ADDED LIST ELEMENT
     if (toggle.checked == false) {
         makeDraggable()
     }
@@ -93,26 +55,26 @@ const addList = () => {
 function addTask() {
     console.log('click')
     // for (let k = 0; k < addTaskButton.length; k++) {
-        const taskButton = addTaskButton[list_items.length - 1]
-        console.log(taskButton)
-        taskButton.addEventListener('click', function () {
-            const newTask = document.createElement('div');
-            newTask.innerHTML = 'Click to enter text'
-            newTask.classList.add('list-item')
-            newTask.setAttribute('draggable', 'true');
-            if (textToggle.checked == true) {
-                newTask.setAttribute('contenteditable', 'false');
-            } else {
-                newTask.setAttribute('contenteditable', 'true');
-            }
-            taskButton.previousElementSibling.append(newTask);
-            list_items = document.querySelectorAll('.list-item');
-            console.log(list_items);
-            if (toggle.checked == false) {
-                console.log('box is unchecked')
-                // makeDraggable();
-            };
-        });
+    const taskButton = addTaskButton[list_items.length - 1]
+    console.log(taskButton)
+    taskButton.addEventListener('click', function () {
+        const newTask = document.createElement('div');
+        newTask.innerHTML = 'Click to enter text'
+        newTask.classList.add('list-item')
+        newTask.setAttribute('draggable', 'true');
+        if (textToggle.checked == true) {
+            newTask.setAttribute('contenteditable', 'false');
+        } else {
+            newTask.setAttribute('contenteditable', 'true');
+        }
+        taskButton.previousElementSibling.append(newTask);
+        list_items = document.querySelectorAll('.list-item');
+        console.log(list_items);
+        if (toggle.checked == false) {
+            console.log('box is unchecked')
+            // makeDraggable();
+        };
+    });
     // };
 };
 
@@ -120,7 +82,7 @@ const textToggle = document.querySelector('#editTextToggle');
 
 
 function appendTask() {
-    for(let z=0; z<addTaskButton.length; z++) {
+    for (let z = 0; z < addTaskButton.length; z++) {
         const newTask = document.createElement('div');
         newTask.innerHTML = 'Click to enter text'
         newTask.classList.add('list-item')
@@ -139,6 +101,7 @@ function appendTask() {
 
 
 function makeDraggable() {
+    // list_items = document.querySelectorAll('.list-item');
     for (let i = 0; i < list_items.length; i++) {
         const item = list_items[i];
 
@@ -182,6 +145,7 @@ function makeDraggable() {
 }
 
 function listDrag() {
+    let lists = document.querySelectorAll('.list');
     lists.forEach(list => {
         list.addEventListener('dragstart', () => {
             list.classList.add('dragging')
@@ -251,6 +215,72 @@ textToggle.addEventListener('click', () => {
         lists.forEach(list => list.children[0].children[0].setAttribute('contenteditable', 'true'))
         editTextOn.style.opacity = '100%';
         editTextOff.style.opacity = '50%';
+    }
+
+})
+
+// EVENT LISTENER FOR TOGGLING DRAG
+// IF CHECK == TRUE, THEN DISABLE MOVING TASKS AND ENABLE MOVING LISTS
+// IF CHECK == FALSE, THEN ENABLE MOVING TASKS AND DISABLE MOVING LISTS
+toggle.addEventListener('click', () => {
+    if (toggle.checked == true) {
+
+        movableTaskText.style.opacity = '50%';
+        movableListText.style.opacity = '100%';
+
+        lists = document.querySelectorAll('.list');
+        // Columns are draggable
+        lists.forEach(list => {
+            list.setAttribute('draggable', 'true');
+        });
+        // List_items are NOT draggable
+        list_items.forEach(list_item => {
+            list_item.setAttribute('draggable', 'false');
+        })
+
+        container.addEventListener('dragover', dropList)
+        listDrag()
+    } else if (toggle.checked == false) {
+
+        movableTaskText.style.opacity = '100%';
+        movableListText.style.opacity = '50%';
+
+        // Columns are NOT draggable
+        lists.forEach(list => {
+            list.setAttribute('draggable', 'false')
+        })
+        // List_items are draggable
+        list_items.forEach(list_item => {
+            list_item.setAttribute('draggable', 'true');
+        })
+
+        container.removeEventListener('dragover', dropList)
+        makeDraggable()
+    }
+})
+
+// EVENT LISTENER FOR TOGGLING EDIT TEXT
+// IF CHECK == TRUE, THEN DISABLE TEXT EDITING
+// IF CHECK == FALSE, THEN ENABLE TEXT EDITING
+textToggle.addEventListener('click', () => {
+    //NEED TO RE-ESTABLISH NODES (list_items and lists) JUST IN CASE ANY NEW ONES WERE ADDED
+    list_items = document.querySelectorAll('.list-item');
+    lists = document.querySelectorAll('.list');
+
+    if (textToggle.checked == true) {
+
+        editTextOn.style.opacity = '50%';
+        editTextOff.style.opacity = '100%';
+
+        list_items.forEach(item => item.setAttribute('contenteditable', 'false'))
+        lists.forEach(list => list.children[0].children[0].setAttribute('contenteditable', 'false'))
+    } else if (textToggle.checked == false) {
+
+        editTextOn.style.opacity = '100%';
+        editTextOff.style.opacity = '50%';
+
+        list_items.forEach(item => item.setAttribute('contenteditable', 'true'))
+        lists.forEach(list => list.children[0].children[0].setAttribute('contenteditable', 'true'))
     }
 
 }) 
