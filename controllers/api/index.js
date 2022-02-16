168a028e64e16a15d18805a7b42b83262bf2f870
Romeo Dixon <romeodixon2nd@gmail.com>
2022-02-16 11:03:35 -0600

    added title to projects page, del html

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,14 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
-const listRoutes = require('./listRoutes');
-const taskRoutes = require('./taskRoutes');
-const noteRoutes = require('./noteRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const projectRoutes = require("./projectRoutes");
+const listRoutes = require("./listRoutes");
+const taskRoutes = require("./taskRoutes");
+const noteRoutes = require("./noteRoutes");
 
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
-router.use('/list', listRoutes);
-router.use('/task', taskRoutes);
-router.use('/note', noteRoutes);
+router.use("/users", userRoutes);
+router.use("/projects", projectRoutes);
+router.use("/list", listRoutes);
+router.use("/task", taskRoutes);
+router.use("/note", noteRoutes);
 
 module.exports = router;
--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -1,60 +1,58 @@
-const list = require('express').Router();
-const { List, Task } = require('../../models');
+const list = require("express").Router();
+const { List, Task } = require("../../models");
 
-list.get('/', async (req, res) => {
-    try {
-        const listData = await List.findAll({
-            include: [{ model: Task }]
-        })
-        res.status(200).json(listData)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
-
-list.post('/', async (req, res) => {
-    // "list_content":"hello",
-    // "position":1,
-    // "project_id":1
-    try {
-        const listData = await List.create(req.body)
-        res.status(200).json(listData)
-    } catch (err) {
-        res.status(500).json(err)
-    }
+list.get("/", async (req, res) => {
+  try {
+    const listData = await List.findAll({
+      include: [{ model: Task }],
+    });
+    res.status(200).json(listData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-list.put('/:id', async (req, res) => {
-    try {
-        const listData = await List.update(req.body, {
-            where: {
-                id: req.params.id
-            },
-        })
-        if (!listData) {
-            res.status(404).json(listData)
-        } else {
-            res.status(200).json(listData)
-        }
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+list.post("/", async (req, res) => {
+ 
+  try {
+    const listData = await List.create(req.body);
+    res.status(200).json(listData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-list.delete('/:id', async (req, res) => {
-    try {
-        const listData = await List.destroy({
-            where: {
-                id: req.params.id
-            },
-        })
-        if (!listData) {
-            res.status(404).json(`list with id=${req.params.id} does not exist`)
-        }
-        res.status(200).json(`list #${req.params.id} has been deleted`)
-    } catch (err) {
-        res.status(500).json(err)
+list.put("/:id", async (req, res) => {
+  try {
+    const listData = await List.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!listData) {
+      res.status(404).json(listData);
+    } else {
+      res.status(200).json(listData);
     }
-})
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-module.exports = list
+list.delete("/:id", async (req, res) => {
+  try {
+    const listData = await List.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!listData) {
+      res.status(404).json(`list with id=${req.params.id} does not exist`);
+    }
+    res.status(200).json(`list #${req.params.id} has been deleted`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = list;
